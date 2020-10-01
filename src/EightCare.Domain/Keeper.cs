580949f99ef3120e67a3d3eb9fe929105787fd53
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-02 00:08:37 +0200

    Add Animal collection to the Keeper

--- src/EightCare.Domain/Keeper.cs
+++ src/EightCare.Domain/Keeper.cs
@@ -1,16 +1,28 @@
+using System.Collections.Generic;
+
 namespace EightCare.Domain
 {
     public sealed class Keeper
     {
+        private readonly List<Animal> _animals;
+
         public string Name { get; }
         public string Email { get; }
         public int Age { get; }
+        public IReadOnlyCollection<Animal> Animals => _animals.AsReadOnly();
 
         public Keeper(string name, string email, int age)
         {
             Name = name;
             Email = email;
             Age = age;
+
+            _animals = new List<Animal>();
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            _animals.Add(animal);
         }
     }
 }
