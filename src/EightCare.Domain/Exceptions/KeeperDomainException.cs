fe7064450b5c65776b96eab105a12532fa61575b
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-06 00:08:52 +0200

    Add DomainException type

---
+++ src/EightCare.Domain/Exceptions/KeeperDomainException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EightCare.Domain.Exceptions
+{
+    public class KeeperDomainException : Exception
+    {
+        public KeeperDomainException()
+        {
+        }
+
+        public KeeperDomainException(string message) : base(message)
+        {
+        }
+
+        public KeeperDomainException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
