bd69749524ab77763a0dc2eb10f48d617c48fda8
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-10 02:11:59 +0200

    Add Feeding Entity

---
+++ src/EightCare.Domain/KeeperAggregate/Feeding.cs
@@ -0,0 +1,19 @@
+﻿using EightCare.Domain.Common;
+using System;
+
+namespace EightCare.Domain.KeeperAggregate
+{
+    public sealed class Feeding : Entity
+    {
+        public DateTime Date { get; private set; }
+        public Feed Feed { get; private set; }
+        public int Count { get; private set; }
+
+        public Feeding(DateTime date, Feed feed, int count)
+        {
+            Date = date;
+            Feed = feed;
+            Count = count;
+        }
+    }
+}
