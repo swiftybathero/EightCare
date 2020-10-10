57fc7ac211045fc20624ece5a24f70e9436f6ec5
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-10 02:10:59 +0200

    Add Feed ValueObject

---
+++ src/EightCare.Domain/Enums/FeedType.cs
@@ -0,0 +1,9 @@
+﻿namespace EightCare.Domain.Enums
+{
+    public enum FeedType
+    {
+        Alive = 0,
+        Wet = 1,
+        Dry = 2
+    }
+}
---
+++ src/EightCare.Domain/KeeperAggregate/Feed.cs
@@ -0,0 +1,28 @@
+﻿using EightCare.Domain.Common;
+using EightCare.Domain.Enums;
+using System.Collections.Generic;
+
+namespace EightCare.Domain.KeeperAggregate
+{
+    public class Feed : ValueObject
+    {
+        public static Feed DubiaRoach = new Feed("Dubia Roach", FeedType.Alive);
+        public static Feed MadagascarRoach = new Feed("Dubia Roach", FeedType.Alive);
+        public static Feed TurkishRoach = new Feed("Dubia Roach", FeedType.Alive);
+
+        public string Name { get; }
+        public FeedType Type { get; }
+
+        public Feed(string name, FeedType type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Name;
+            yield return Type;
+        }
+    }
+}
