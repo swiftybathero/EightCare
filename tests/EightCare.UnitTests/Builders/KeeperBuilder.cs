071734b02e24a698f6c3edf52a931d213e64932e
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-10 14:39:44 +0200

    Add Keeper builder unit tests utility class

---
+++ tests/EightCare.UnitTests/Builders/KeeperBuilder.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using EightCare.Domain.KeeperAggregate;
+using System.Linq;
+
+namespace EightCare.UnitTests.Builders
+{
+    public class KeeperBuilder
+    {
+        private readonly IFixture _fixture;
+        private Keeper _keeper;
+
+        public KeeperBuilder()
+        {
+            _fixture = new Fixture();
+        }
+
+        public KeeperBuilder Build(string name, string email, int age)
+        {
+            _keeper = new Keeper(name, email, age);
+
+            return this;
+        }
+
+        public KeeperBuilder BuildDefault()
+        {
+            _keeper = CreateDefaultKeeper();
+
+            return this;
+        }
+
+        public Keeper Create()
+        {
+            return _keeper ?? CreateDefaultKeeper();
+        }
+
+        public KeeperBuilder WithAnimals(params int[] animalIds)
+        {
+            if (animalIds.Length == 0)
+                animalIds = _fixture.CreateMany<int>().ToArray();
+
+            foreach (var animalId in animalIds)
+            {
+                var createdAnimal = _keeper.AddNewAnimal(_fixture.Create<string>(), _fixture.Create<string>());
+                createdAnimal.SetId(animalId);
+            }
+
+            return this;
+        }
+
+        private Keeper CreateDefaultKeeper()
+        {
+            return _fixture.Create<Keeper>();
+        }
+    }
+}
