e0f24f2ace28e6d04c557e9c202d6a66303acbdd
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-12 00:16:07 +0200

    Add IKeeperRepository to domain layer

---
+++ src/EightCare.Domain/KeeperAggregate/Abstractions/IKeeperRepository.cs
@@ -0,0 +1,12 @@
+﻿using EightCare.Domain.Common;
+
+namespace EightCare.Domain.KeeperAggregate.Abstractions
+{
+    public interface IKeeperRepository : IRepository<Keeper>
+    {
+        Keeper GetById(int keeperId);
+        void Add(Keeper keeper);
+        void Update(Keeper keeper);
+        void Delete(Keeper keeper);
+    }
+}
