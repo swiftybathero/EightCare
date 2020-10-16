955ad941d638e4674238f6ff76090bbcdd3c7d29
Paweł Klatt <klatt.pawel@gmail.com>
2020-10-17 01:04:42 +0200

    Add InMemoryKeeperRepository unit tests

--- tests/EightCare.UnitTests/Repositories/InMemoryKeeperRepositoryTests.cs
+++ tests/EightCare.UnitTests/Repositories/InMemoryKeeperRepositoryTests.cs
@@ -7,6 +7,7 @@ using EightCare.UnitTests.Extensions;
 using FluentAssertions;
 using NSubstitute;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -43,6 +44,89 @@ namespace EightCare.UnitTests.Repositories
             // Assert
             foundKeeper.Should().NotBeNull();
             foundKeeper.Should().BeEquivalentTo(expectedKeeper, options => options.ComparingByMembers<Keeper>());
+        }
+
+        [Fact]
+        public void Add_ShouldInvokeContextAdd()
+        {
+            // Arrange
+            var keeper = _fixture.Create<Keeper>();
+
+            _keeperInMemoryContext.Keepers.Returns(new List<Keeper>());
+
+            // Act
+            _inMemoryKeeperRepository.Add(keeper);
+
+            // Assert
+            _keeperInMemoryContext.Keepers
+                                  .Should()
+                                  .ContainEquivalentOf(keeper, options => options.ComparingByMembers<Keeper>());
+        }
+
+        [Fact]
+        public void Update_ExistingKeeper_ShouldUpdateValues()
+        {
+            // Arrange
+            var existingKeeperId = _fixture.Create<Guid>();
+            var otherExistingKeeperIds = _fixture.CreateMany<Guid>().ToArray();
+            var keeperIds = otherExistingKeeperIds.Concat(new List<Guid> { existingKeeperId }).ToArray();
+
+            var keepers = _fixture.CreateManyWithIds<Keeper>(keeperIds).ToList();
+            var newKeeperData = _fixture.CreateManyWithIds<Keeper>(existingKeeperId).First();
+
+            _keeperInMemoryContext.Keepers.Returns(keepers);
+
+            // Act
+            _inMemoryKeeperRepository.Update(newKeeperData);
+
+            // Assert
+            _inMemoryKeeperRepository.GetById(existingKeeperId)
+                                     .Should()
+                                     .BeEquivalentTo(newKeeperData, options => options.ComparingByMembers<Keeper>());
+        }
+
+        [Fact]
+        public void Update_KeeperWithoutId_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var keeperWithoutId = _fixture.Create<Keeper>();
+
+            // Act // Assert
+            _inMemoryKeeperRepository.Invoking(x => x.Update(keeperWithoutId))
+                                     .Should()
+                                     .Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Delete_ExistingKeeper_ShouldDeleteKeeper()
+        {
+            // Arrange
+            var existingKeeperId = _fixture.Create<Guid>();
+            var otherExistingKeeperIds = _fixture.CreateMany<Guid>().ToArray();
+            var keeperIds = otherExistingKeeperIds.Concat(new List<Guid> { existingKeeperId }).ToArray();
+
+            var keepers = _fixture.CreateManyWithIds<Keeper>(keeperIds).ToList();
+            var keeperToDelete = _fixture.CreateManyWithIds<Keeper>(existingKeeperId).First();
+
+            _keeperInMemoryContext.Keepers.Returns(keepers);
+
+            // Act
+            _inMemoryKeeperRepository.Delete(keeperToDelete);
+
+            // Assert
+            _inMemoryKeeperRepository.GetById(existingKeeperId).Should().BeNull();
+        }
+
+        [Fact]
+        public void Delete_KeeperWithoutId_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var keeperWithoutId = _fixture.Create<Keeper>();
+
+            // Act // Assert
+            _inMemoryKeeperRepository.Invoking(x => x.Delete(keeperWithoutId))
+                                     .Should()
+                                     .Throw<ArgumentException>();
         }
     }
 }
