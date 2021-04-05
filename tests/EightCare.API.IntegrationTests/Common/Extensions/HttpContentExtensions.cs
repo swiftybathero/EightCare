e4a835c4da04c1cf80794b6487633b8b797c8205
Paweł Klatt <klatt.pawel@gmail.com>
2021-04-05 22:37:28 +0200

    Add KeepersController Integration Tests

---
+++ tests/EightCare.API.IntegrationTests/Common/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Dynamic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EightCare.API.IntegrationTests.Common.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<string> GetIdAsync(this HttpContent content)
+        {
+            dynamic responseObject = await content.ReadFromJsonAsync<ExpandoObject>();
+
+            return responseObject?.id.ToString();
+        }
+    }
+}
