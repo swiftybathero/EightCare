6138a09d7ae91aa0aa4315c77f7596ce3a29330f
Paweł Klatt <klatt.pawel@gmail.com>
2021-04-23 01:47:23 +0200

    Add RegisterKeeper Unit Test

---
+++ tests/EightCare.API.UnitTests/Controllers/KeepersControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using EightCare.API.Controllers;
+using EightCare.Application.Keepers.Commands.RegisterKeeper;
+using MediatR;
+using NSubstitute;
+using Xunit;
+
+namespace EightCare.API.UnitTests.Controllers
+{
+    public class KeepersControllerTests
+    {
+        private readonly KeepersController _keepersController;
+        private readonly IMediator _mediator;
+        private readonly IFixture _fixture;
+
+        public KeepersControllerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoNSubstituteCustomization());
+
+            _mediator = _fixture.Freeze<IMediator>();
+            _keepersController = new KeepersController(_fixture.Create<IMediator>());
+        }
+
+        [Fact]
+        public async Task RegisterKeeper_ShouldSendRegisterCommand()
+        {
+            // Arrange
+            var registerKeeperCommand = _fixture.Create<RegisterKeeperCommand>();
+
+            // Act
+            await _keepersController.RegisterKeeper(registerKeeperCommand);
+
+            // Assert
+            await _mediator.Received(1).Send(Arg.Is(registerKeeperCommand));
+        }
+    }
+}
